766ad04b80843e09c347cc7126e22220652f415a
“Ricky <grickyh@gmail.com>
2021-06-24 06:13:19 +0100

    using State

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CaughtPokemon = () => {
     const date = new Date().toLocaleString();
-    return <p>Caught 0 Pokemon on {date}</p>;
+    // use State functionality to update pokemon caught
+    const [caught, setCaught] = useState(0);
+
+    function catchPokemon() {
+      setCaught(caught + 1);
+    }
+    return (
+    <div>
+      {/*Onclick call the function and update caught variable by one */}
+    <button onClick={catchPokemon}>Click me!</button>
+    <p>Caught {caught} Pokemon on {date}</p>
+    </div>
+    );
   };
 
   export default CaughtPokemon;
