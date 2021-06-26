77551d4ad4f8be5d672ea86339ba9f229123968b
“Ricky <grickyh@gmail.com>
2021-06-26 17:42:36 +0100

    caught pokemon function

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
+// import array of all Pokemon
+const pokemon = require("pokemon");
 
 const CaughtPokemon = () => {
     const date = new Date().toLocaleString();
     // use State functionality to update pokemon caught
-    const [caught, setCaught] = useState(0);
+    const [caught, setCaught] = useState([]);
 
     function catchPokemon() {
-      setCaught(caught + 1);
+      setCaught(caught.concat(pokemon.random()));
     }
     return (
     <div>
       {/*Onclick call the function and update caught variable by one */}
     <button onClick={catchPokemon}>Catch Pokemon!</button>
-    <p>Caught {caught} Pokemon on {date}</p>
-    </div>
+    <p>Caught {caught.length} Pokemon on {date}</p>
+    <ul>{caught.map((pokemon, index) => (
+      <li key={index}>{pokemon}</li>
+    ))}</ul>
+   </div>
     );
   };
 
